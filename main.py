18a76209eb283fc8e64c6e34b2134ec4ca0d3e45
GeorgiyEgorov <Stetszei80@gmail.com>
2023-01-27 18:03:51 +0400

    modified main

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print('hello ')
+name = "Georgiy"
+age = 27
+age = int(age) - 18
+print(age)
+print('georgiy starshe na', age, 'moloje georgia na ', age)15
+
+
+
+d = input('введите GOD')
+c = input('введите BOZ')
+d, c = c, d
+print('d =', d)
+print('c = ', c)
+
+print('reshaem uravnenie ax2+bx+c=0')
+a = input('znachenie a')
+b = input('znachenie b')
+c = input('znachenie c')
+a = float(a)
+b = float(b)
+c = float(c)
+discriminant = b**2 - 4*a*c
+print('discriminant' + str(discriminant))
+if discriminant < 0:
+    print('kornei net')
+elif discriminant == 0:
+    x = -b/(2 * a)
+    print('x= ' + str(x))
+else:
+    x1 = (-b + discriminant ** 0.5) / (2 * a)
+    x2 = (-b - discriminant ** 0.5) / (2 * a)
+    print('x₁ = ' + str(x1))
+    print('x₂ = ' + str(x2))
+
+
+
+
+
+
